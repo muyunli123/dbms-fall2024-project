ae2fd9d89f19f900f1e4f99f2d4972cb739e5151
muyunli123 <ml8502@nyu.edu>
2024-12-20 07:23:07 +0000

    deleted connection string

--- service/db_utils.py
+++ service/db_utils.py
@@ -6,6 +6,6 @@ def get_db_connection():
     Establish a connection to the database using a connection string.
     Update the connection string with your database details.
     """
-    connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:carrentalfords.database.windows.net,1433;Database=CarRental;Uid=carrental;Pwd={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    connection_string = ""
     conn = odbc.connect(connection_string)
     return conn
